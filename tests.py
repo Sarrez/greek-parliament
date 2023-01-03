f294209a8c0ddb9ab68e17e8d5e225c9bfa45f73
Sarrez <mksarr99@gmail.com>
2023-01-03 17:18:38 +0200

    tests for index

---
+++ tests.py
@@ -0,0 +1,34 @@
+from indexer import create_index
+import unittest
+import pandas as pd
+
+class TestIndex(unittest.TestCase):
+    def test_num_tokens(self):
+        dataframe = pd.read_csv('test.csv', encoding='utf8', chunksize=3)
+        tokens = create_index(dataframe = dataframe)
+        num_tokens = len(tokens.keys())
+        self.assertEqual(num_tokens, 6)
+
+    def test_posting_list_length(self):
+        dataframe = pd.read_csv('test.csv', encoding='utf8', chunksize=3)
+        tokens = create_index(dataframe = dataframe)
+        posting_list = len(tokens['αυτ']['postinglist'].keys())
+        self.assertEqual(posting_list, 2)
+        posting_list = len(tokens['ομιλ']['postinglist'].keys())
+        self.assertEqual(posting_list, 3)
+    
+    def test_posting_list_docs(self):
+        dataframe = pd.read_csv('test.csv', encoding='utf8', chunksize=3)
+        tokens = create_index(dataframe = dataframe)
+        #print(tokens)
+        correct = {'2':1}
+        posting_list = tokens['τριτ']['postinglist']
+        #print(posting_list)
+        self.assertEquals(correct, posting_list)
+        posting_list = tokens['αυτ']['postinglist']
+        correct = {'0':1, '1':1}
+
+
+
+if __name__ == '__main__':
+    unittest.main()
